04423698a3e5d3555ed5ed5d8baa77470f585747
Desu123! <desu6262@gmail.com>
2026-02-18 13:44:48 +0300

    Add shared module re-exports for build

---
+++ shared/cardGenerator.ts
@@ -0,0 +1,5 @@
+import type { BingoCard, Cell } from '@/types/game';
+import { generateCard, generateCards } from '@/lib/cardGenerator';
+
+export type { BingoCard, Cell };
+export { generateCard, generateCards };
